038d168be1aaaf5f4eab41618c9e11b3910f9aa5
Monimoy Bujarbaruah <monimoy_07@yahoo.co.in>
2021-04-23 13:55:57 -0700

    Update obtain_mat.m
    indents fixed

--- obtain_mat.m
+++ obtain_mat.m
@@ -1,14 +1,14 @@
 %% Forming Big Matrices for Robust Optimization (See https://www.sciencedirect.com/science/article/pii/S0005109806000021)
-% Monimoy Bujarbaruah
+%  Monimoy Bujarbaruah
 
 function [capA, capE, capB, capC, capD, Aw_batch, Bu_batch, A_batch] = obtain_mat(A,B,C,D,Xn,nx,nu,N,dim_t) 
 
     capA = eye(nx);
-    
+
     for k=1:N
         capA = [capA;A^k];
     end
-   
+
     matE(:,:,1) = [eye(nx),zeros(nx,nx*(N-1))];
     capE = [zeros(nx,nx*N);matE(:,:,1)];
 
@@ -21,10 +21,10 @@ function [capA, capE, capB, capC, capD, Aw_batch, Bu_batch, A_batch] = obtain_ma
     capC = blkdiag(kron(eye(N),C),Xn.A); 
     capD = [kron(eye(N),D);zeros(dim_t-size(kron(eye(N),D),1),nu*N)];
 
-     
-   %% Batch matrices for simulation check
-   Aw_batch = capE(nx+1:end,:); 
-   Bu_batch = capB(nx+1:end,:);
-   A_batch  =  capA(nx+1:end,:);
+
+    %% Batch matrices for simulation check
+    Aw_batch = capE(nx+1:end,:); 
+    Bu_batch = capB(nx+1:end,:);
+    A_batch  =  capA(nx+1:end,:);
      
 end
